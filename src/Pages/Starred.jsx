c37d8e7919fc72b4484a8897aebf5dc2a10d61ef
kaushal patil <patilkaushl925@gmail.com>
2023-08-20 14:56:02 +0530

    added react router dom to app.jsx

---
+++ src/Pages/Starred.jsx
@@ -0,0 +1,14 @@
+const Starred = () => {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem
+      accusamus, modi culpa ad voluptatem vitae quasi soluta dolores aspernatur
+      ipsam eum nisi temporibus nostrum cupiditate, porro, aliquam vero.
+      Voluptatibus esse ducimus hic ex accusantium adipisci alias, error dolores
+      molestias cupiditate maxime? Quasi delectus incidunt deserunt asperiores
+      totam ex, quibusdam veritatis.
+    </div>
+  );
+};
+
+export default Starred;
